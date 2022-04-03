a375d825ea283da04c9b9e317732333e0924685a
PopFang <fangz5@rpi.edu>
2022-04-03 13:44:54 -0400

    fix revisitOTS

---
+++ plot/ColorPalette.m
@@ -0,0 +1,17 @@
+classdef ColorPalette
+    % Define my favorite colors
+   properties
+        Blue        = [   9, 132, 227] / 255;
+        Dark        = [  52,  73,  94] ./ 255;
+        Green       = [   0, 184, 148] / 255;
+        Red         = [ 255, 118, 117] / 255 .* .8;
+        Yellow      = [ 253, 203, 110] / 255;
+        Grey        = [ .7, .7, .7];
+   end
+   methods
+      function colors = getPalette(obj)
+          %  get the palette for plot
+         colors   = {obj.Dark, obj.Green, obj.Red, obj.Blue, obj.Yellow, obj.Grey};
+      end
+   end
+end
--- revisitOTS.m
+++ revisitOTS.m
@@ -1,113 +1,37 @@
-function []=revisitOTS(fig, dataset, roi, group)
+function revisitOTS(fig, ds, roi)
+%{
+    Function for the supplement document revisitOTS
+    ds: dataset, default 1
+    roi: region of interest, default 1, v1
+    Inputs: fig, the corresponding figures for the document 
+            S means section, H means the hypothesis put forward in the
+            very section. 'S2H1' means section 2 hypothesis 1.
+            - 'S2H1': compare the new and old fitlers
+            - 'S2H2.1': compare the E with new and old filters
+            - 'S2H2.2': compare the density effect of S with new and old filters varying w
+            - 'S2H3': compare the contrast effect
+            - 'S2H4.0': get the old contrast energy for all target parameters E.  
+            - 'S2H4': compare the prediction of the old and new filters,
+            make sure the 'H4.0' has been run at least once, before making
+            this figure. 
+            - 'S3H1':  check the std of the new filter
+              
+%}
 
-% Some variables
-switch dataset
-    case 1
-        switch group
-            case 'den'
-                cls = [ 6, 7, 8, 9, 10; 1, 2, 3, 4, 5];
-            case 'con'
-                cls = [ 35, 36, 8, 37, 38; 47, 48, 3, 49, 50];
-            case 'tar'
-                cls = [ 1, 2, 3, 4, 5;...
-                    6, 7, 8, 9, 10; ...
-                    47, 48, 3, 49, 50;...
-                    35, 36, 8, 37, 38];
-        end
-    case 2
-        switch group
-            case 'den'
-                cls = [ 6, 7, 8, 9, 10; 1, 2, 3, 4, 5];
-            case 'con'
-                cls = [ 33, 34, 7, 35, 36; 45, 46, 2, 47, 48];
-            case 'tar'
-                cls = [ 1, 2, 3, 4, 5;...
-                    6, 7, 8, 9, 10;...
-                    45, 46, 2, 47, 48;...
-                    33, 34, 7, 35, 36];
-        end
-    case {3,4}
-        switch group
-            case 'den'
-                cls = [ 26, 27, 28, 29; 39, 38, 37, 36];
-            case 'con'
-                cls = [ 9, 10, 11, 12; 34, 33, 32, 31];
-            case 'tar'
-                cls = [ 39, 38, 37, 36;...
-                    26, 27, 28, 29;...
-                    34, 33, 32, 31;...
-                    9, 10, 11, 12 ];
-        end
-end
-ep          = 5;
-Blue        = [   9, 132, 227] / 255;
-Dark        = [  52,  73,  94] ./ 255;
-Green       = [   0, 184, 148] / 255;
-Red         = [ 255, 118, 117] / 255 .* .8;
-Yellow      = [ 253, 203, 110] / 255;
-Grey        = [ .7, .7, .7];
-filter_cpd  = 3; % the images were band-passed at 3 cycles per degree
-fovs        = 12.5 * [1, 3/2, 1, 1]; % deg (the second data set had a larger field of view than the others)
-fov         = fovs(dataset);
-numpix      = 150;
-pixperdeg   = numpix / fov;
-ppc         = pixperdeg/filter_cpd; % pixels per cycle
-support     = 6; % cycles per filter
-o = linspace(0,pi, 9);
-thetavec = o(1:end-1);
+% get default input
+if (nargin < 3), roi = 1; end
+if (nargin < 2), ds = 1; end
 
-% Get stimuli
-S = dataloader( stdnormRootPath, 'stimuli', 'all', dataset, roi);
-S_gra = S( :, :, ep, 3);
-S_sna = S( :, :, ep, 8);
-
-% Get filters
-[ Gabor_c, Gabor_s]=makeGaborFilter(ppc, thetavec, support);
-[ oGabor_c, oGabor_s] = makeOldFilter(ppc, thetavec, support);
-
-% Get contrast energy
-E_gra = squeeze(Icontrast(S_gra, Gabor_c, Gabor_s, ppc, thetavec));
-E_sna = squeeze(Icontrast(S_sna, Gabor_c, Gabor_s, ppc, thetavec));
-oE_gra = squeeze(Icontrast(S_gra, oGabor_c, oGabor_s, ppc, thetavec));
-oE_sna = squeeze(Icontrast(S_sna, oGabor_c, oGabor_s, ppc, thetavec));
-
-% check the sum CE at different orientations
-Eori_gra = squeeze(mean(E_gra,[1,2]));
-Eori_sna = squeeze(mean(E_sna,[1,2]));
-oEori_gra = squeeze(mean(oE_gra,[1,2]));
-oEori_sna = squeeze(mean(oE_sna,[1,2]));
-
-% Get OTS normalized energy
-% get filters
-sigma_p=.1;
-sigma_g=.85;
-sigma_s=.01;
-sz = round(size(E_gra, 1) / 20)*2;
-F = kernel_weight( sigma_p, sigma_g, sigma_s, sz );
-% get normalizer
-Z_gra  = calc_z( E_gra, F);
-Z_sna  = calc_z( E_sna, F);
-oZ_gra = calc_z( oE_gra, F);
-oZ_sna = calc_z( oE_sna, F);
-
-% check the contrast effect
-% dim1: old or new; 2
-% dim2: gratings or snakes; 2
-% dim3: contrast level; 5
-%cls = [ 35, 36, 8, 37, 38; 47, 48, 3, 49, 50];
-
-E_cls  = cell([2,size(cls)]);
-
-for i=1:size(cls,1)
-    for j=1:size(cls,2)
-        [E, oE] = idx2CE(cls(i,j));
-        E_cls{1,i,j} = E;
-        E_cls{2,i,j} = oE;
-    end
-end
+% get color
+viz = ColorPalette(); 
 
 switch fig
-    case 'GaborFilter'
+    
+    case 'S2H1'
+        
+        % get filter t
+        [ Gabor_c, Gabor_s] = getFilters( 'new', ds) ;
+        [ oGabor_c, oGabor_s] = getFilters( 'old', ds) ;
         
         % visualize the filters
         for i = 1:8
@@ -125,7 +49,17 @@ switch fig
             title(sprintf('range:[%0.2f, %.2f]', [min(x(:)), max(x(:))]))
         end
         
-    case 'Example'
+    case 'S2H2.1'
+        
+        % Get contrast energy
+        [E_sna, oE_sna] = idx2CE( 3, ds, roi);
+        [E_gra, oE_gra] = idx2CE( 8, ds, roi);
+        
+        % check the sum CE at different orientations
+        Eori_gra = squeeze(mean(E_gra,[1,2,4]));
+        Eori_sna = squeeze(mean(E_sna,[1,2,4]));
+        oEori_gra = squeeze(mean(oE_gra,[1,2,4]));
+        oEori_sna = squeeze(mean(oE_sna,[1,2,4]));
         
         % for orientation energy figure
         figure();
@@ -136,10 +70,10 @@ switch fig
         Eori_g_rat = Eori_gra ./ oEori_gra;
         text(1:length(Eori_g_rat),oEori_gra, num2str(Eori_g_rat,'%.2f'),...
             'vert','bottom','horiz','center','FontSize', 8);
-        b1(1).FaceColor = Blue;
-        b1(1).EdgeColor = Blue;
-        b1(2).FaceColor = Blue .* .7;
-        b1(2).EdgeColor = Blue .* .7;
+        b1(1).FaceColor = viz.Blue;
+        b1(1).EdgeColor = viz.Blue;
+        b1(2).FaceColor = viz.Blue .* .7;
+        b1(2).EdgeColor = viz.Blue .* .7;
         xlabel( 'Orientation','FontSize', 12)
         ylabel( 'Averaged contrast energy','FontSize', 12)
         legend( 'New', 'Old')
@@ -150,10 +84,10 @@ switch fig
         Eori_s_rat = Eori_sna ./ oEori_sna;
         text(1:length(Eori_s_rat),oEori_sna, num2str(Eori_s_rat,'%.2f'),...
             'vert','bottom','horiz','center','FontSize', 8);
-        b2(1).FaceColor = Red;
-        b2(1).EdgeColor = Red;
-        b2(2).FaceColor = Red .* .7;
-        b2(2).EdgeColor = Red .* .7;
+        b2(1).FaceColor = viz.Red;
+        b2(1).EdgeColor = viz.Red;
+        b2(2).FaceColor = viz.Red .* .7;
+        b2(2).EdgeColor = viz.Red .* .7;
         xlabel( 'Orientation','FontSize', 12)
         ylabel( 'Averaged contrast energy','FontSize', 12)
         legend( 'New', 'Old','FontSize', 12)
@@ -165,18 +99,39 @@ switch fig
         Eori_rat = [ sum(Eori_gra), sum(Eori_sna)] ./ [ sum(oEori_gra), sum(oEori_sna)];
         text(1:length(Eori_rat),[ sum(oEori_gra), sum(oEori_sna)], num2str(Eori_rat','%.2f'),...
             'vert','bottom','horiz','center','FontSize', 8);
-        b3(1).FaceColor = Grey;
-        b3(1).EdgeColor = Grey;
-        b3(2).FaceColor = Grey.* .7;
-        b3(2).EdgeColor = Grey.* .7;
+        b3(1).FaceColor = viz.Grey;
+        b3(1).EdgeColor = viz.Grey;
+        b3(2).FaceColor = viz.Grey.* .7;
+        b3(2).EdgeColor = viz.Grey.* .7;
         ylabel( 'Averaged contrast energy','FontSize', 12)
         legend( 'New', 'Old','FontSize', 12)
         title( 'Sum over orientation');
         xticks([ 1, 2])
         xticklabels({'Gratings','Snakes'})
         
-    case 'Effect'
         
+    case { 'S2H2.2', 'S2H3'}
+        
+        % check the density effect
+        % dim1: old or new; 2
+        % dim2: gratings or snakes; 2
+        % dim3: contrast level; 5
+        %cls = [ 35, 36, 8, 37, 38; 47, 48, 3, 49, 50];
+        switch fig; case 'S2H2.2'; group='dens'; case 'S2H3'; group='con'; end
+        cls = getDataCls( ds, group) ;
+        E_cls  = cell([2,size(cls)]);
+        ep = 5;
+        for i=1:size(cls,1)
+            for j=1:size(cls,2)
+                [E, oE] = idx2CE(cls(i,j), ds, roi, ep);
+                E_cls{1,i,j} = E;
+                E_cls{2,i,j} = oE;
+            end
+        end
+        
+        % Get OTS normalized energy
+        % get filters
+        F = getOTSFilters( E);
         sum_E  = nan([2,size(cls)]);
         sum_Z  = nan([2,size(cls)]);
         sum_D  = nan([2,size(cls)]);
@@ -192,12 +147,11 @@ switch fig
                     D = E ./ ( 1 + w * Z);
                     sum_D(i,j,k) = mean(D(:));
                 end
-                
             end
         end
         % visualize the filters
         sum_vals = { sum_E, sum_Z, sum_D};
-        colors = [ Blue; Red; Blue.*.7; Red.*.7];
+        colors = [ viz.Blue; viz.Red; viz.Blue.*.7; viz.Red.*.7];
         titles = {'summed E', 'summed Z', 'S',...
             'scaled summed E', 'scaled summed Z', 'scaled S'};
         for k=1:length(sum_vals)
@@ -256,12 +210,12 @@ switch fig
                         x = g(i) .* mean(D, [1,2]).^n(i);
                         sum_D(i,j,k,wi) = mean(x(:));
                     end
-                    
                 end
             end
         end
         % visualize the filters
-        colors = [ Blue; Red; Blue.*.7; Red.*.7];
+        colors = [ viz.Blue; viz.Red; viz.Blue.*.7; viz.Red.*.7];
+        x  = sum_D;
         for k=1:length(ws)
             w = ws(k);
             subplot( 2, 5, k)
@@ -282,58 +236,14 @@ switch fig
             xticklabels('')
             
         end
-        sgtitle(sprintf('Scaled s with varied w, DS=%d,V=%d', [dataset, roi]),'FontSize', 14', 'FontWeight', 'Bold')
+        titleStr = sprintf('Scaled s with varied w, DS=%d,V=%d, %s effect', [ds, roi, group]);
+        sgtitle( titleStr,'FontSize', 14', 'FontWeight', 'Bold')
         hold off
         
-    case 'bestParams'
         
-        target = dataloader( stdnormRootPath, 'BOLD_target', 'all', dataset, roi);
-        params = exp([ 10.27, 20.16, 0.7706; 10.27, 20.16, 0.7706]);
-        x = nan([3,size(cls)]);
-        x(3,:,:) = target(cls);
-        m = oriSurroundModel( 'fmincon', 1);
-        height = size(E,1);
-        m = m.disk_weight( m, height);
-        for i=1:2
-            for j=1:size(cls,1)
-                for k=1:size(cls,2)
-                    E = E_cls{i,j,k};
-                    Z = calc_z( E, F);
-                    D = E ./ ( 1 + params(i,1) * Z);
-                    D = bsxfun( @times, D, m.receptive_weight);
-                    x(i,j,k) = mean(params(i,2) .* mean(D, [1,2,3]).^params(i,3));
-                end
-                
-            end
-        end
+    case 'S2H4.0'
         
-        % show predictions
-        figure()
-        colors = [ Red; Blue; Red; Blue; Red.*.7; Blue.*.7; Red.*.7; Blue.*.7];
-        for j=1:size(x,2)
-            l = size(x,3);
-            data = bar((j-1)*(l+1)+1:j*(l+1)-1, squeeze(x(3,j,:)),...
-                'FaceColor', Grey,...
-                'EdgeColor', Grey);
-            hold on
-            new = plot((j-1)*(l+1)+1:j*(l+1)-1, squeeze(x(1,j,:)),...
-                '-o', 'MarkerSize', 5,...
-                'MarkerEdgeColor', colors(j,:), ...
-                'MarkerFaceColor', colors(j,:),...
-                'LineWidth', 3,...
-                'Color', colors(j,:));
-            hold on
-            old = plot((j-1)*(l+1)+1:j*(l+1)-1, squeeze(x(2,j,:)),...
-                '--','LineWidth', 2,...
-                'Color', colors(j+size(x,2),:));
-        end
-        legend( [data,new,old], {'data','new','old'}, 'FontSize', 13)
-        xticklabels('')
-        hold off
-        
-    case 'preOlddata'
-        
-        % get target data 
+        % get target data
         datasets  = [1, 4];
         
         % save address
@@ -378,7 +288,7 @@ switch fig
             clear Z
         end
         
-    case 'H4'
+    case 'S2H4'
         
         % variables
         datasets  = [1, 4];
@@ -441,67 +351,148 @@ switch fig
                 err  = BOLD_data( 2, ds_idx, roi, 1:len);
                 % generate plot
                 plot_BOLD( pred, data, err, ds, [1,4], target, true);
-            if i == length(rois)
-                subplot(length(datasets), length(rois)+1, i+1)
-                plot( 1:2,[nan, nan], '-o',... 
-                    'MarkerSize', 4.5,...
-                    'MarkerEdgeColor', Dark, ...
-                    'MarkerFaceColor', Dark,...
-                    'LineWidth', 1.5,...
-                    'Color', Dark);
-                hold on
-                plot( 1:2,[nan, nan], '-o',... 
-                    'MarkerSize', 4.5,...
-                    'MarkerEdgeColor', Blue, ...
-                    'MarkerFaceColor', Blue,...
-                    'LineWidth', 1.5,...
-                    'Color', Blue);
-               legend({'OLD', 'NEW'}, 'Location', 'East', 'FontSize', 13)
-               axis( 'off' )
-               hold off 
-            end
+                if i == length(rois)
+                    subplot(length(datasets), length(rois)+1, i+1)
+                    plot( 1:2,[nan, nan], '-o',...
+                        'MarkerSize', 4.5,...
+                        'MarkerEdgeColor', viz.Dark, ...
+                        'MarkerFaceColor', viz.Dark,...
+                        'LineWidth', 1.5,...
+                        'Color', viz.Dark);
+                    hold on
+                    plot( 1:2,[nan, nan], '-o',...
+                        'MarkerSize', 4.5,...
+                        'MarkerEdgeColor', viz.Blue, ...
+                        'MarkerFaceColor', viz.Blue,...
+                        'LineWidth', 1.5,...
+                        'Color', viz.Blue);
+                    legend({'OLD', 'NEW'}, 'Location', 'East', 'FontSize', 13)
+                    axis( 'off' )
+                    hold off
+                end
             end
         end
 end
+end
 
+function F = getOTSFilters( E)
+sigma_p=.1;
+sigma_g=.85;
+sigma_s=.01;
+sz = round(size(E, 1) / 20)*2;
+F = kernel_weight( sigma_p, sigma_g, sigma_s, sz );
+end
 
-    function [E, oE]= idx2CE( idx)
-        
-        filter_cpd  = 3; % the images were band-passed at 3 cycles per degree
-        fovs        = 12.5 * [1, 3/2, 1, 1]; % deg (the second data set had a larger field of view than the others)
-        fov         = fovs(dataset);
-        numpix      = 150;
-        pixperdeg   = numpix / fov;
-        ppc         = pixperdeg/filter_cpd; % pixels per cycle
-        support     = 6; % cycles per filter
-        o = linspace(0,pi, 9);
-        thetavec = o(1:end-1);
-        nO=length(thetavec);
-        padsize = ppc * support;
-        nX = numpix + padsize*2;
-        
-        S = dataloader( stdnormRootPath, 'stimuli', 'all', dataset, roi);
+function [E, oE]= idx2CE( idx, dataset, roi, ep)
+
+if (nargin < 4), ep = 0; end
+
+filter_cpd  = 3; % the images were band-passed at 3 cycles per degree
+fovs        = 12.5 * [1, 3/2, 1, 1]; % deg (the second data set had a larger field of view than the others)
+fov         = fovs(dataset);
+numpix      = 150;
+pixperdeg   = numpix / fov;
+ppc         = pixperdeg/filter_cpd; % pixels per cycle
+support     = 6; % cycles per filter
+o = linspace(0,pi, 9);
+thetavec = o(1:end-1);
+nO=length(thetavec);
+padsize = ppc * support;
+nX = numpix + padsize*2;
+
+S = dataloader( stdnormRootPath, 'stimuli', 'all', dataset, roi);
+
+if ep
+        epvec = ep;
+        E = nan(nX, nX, nO, length(epvec));
+        oE = nan(nX, nX, nO, length(epvec));
+else 
+        epvec = 1:9;
         E = nan(nX, nX, nO, 9);
         oE = nan(nX, nX, nO, 9);
-        for iep=1:9
-            s = S( :, :, iep, idx);
-            padstimulus=zeros(nX, nX);
-            padstimulus(padsize+(1:numpix),padsize+(1:numpix))=s;
-            s=padstimulus;
-            
-            % Get filters
-            [ Gabor_c, Gabor_s]=makeGaborFilter(ppc, thetavec, support);
-            [ oGabor_c, oGabor_s] = makeOldFilter(ppc, thetavec, support);
-            
-            % Get contrast energy
-            e = squeeze(Icontrast(s, Gabor_c, Gabor_s, ppc, thetavec));
-            oe = squeeze(Icontrast(s, oGabor_c, oGabor_s, ppc, thetavec));
-            
-            % add to the matrix
-            E( :, :, :, iep) = e;
-            oE( :, :, :, iep) = oe;
-        end
+end
+    for i=1:length(epvec)
+        iep = epvec(i);
+        s = S( :, :, iep, idx);
+        padstimulus=zeros(nX, nX);
+        padstimulus(padsize+(1:numpix),padsize+(1:numpix))=s;
+        s=padstimulus;
+
+        % Get filters
+        [ Gabor_c, Gabor_s]=getFilters( 'new', dataset);
+        [ oGabor_c, oGabor_s] = getFilters( 'old', dataset);
+
+        % Get contrast energy
+        e = squeeze(Icontrast(s, Gabor_c, Gabor_s, ppc, thetavec));
+        oe = squeeze(Icontrast(s, oGabor_c, oGabor_s, ppc, thetavec));
+
+        % add to the matrix
+        E( :, :, :, i) = e;
+        oE( :, :, :, i) = oe;
     end
+    
+    E = squeeze(E);
+    oE = squeeze(oE);
+end
+
+function cls = getDataCls(ds, group )
+switch ds
+    case 1
+        switch group
+            case 'den'
+                cls = [ 6, 7, 8, 9, 10; 1, 2, 3, 4, 5];
+            case 'con'
+                cls = [ 35, 36, 8, 37, 38; 47, 48, 3, 49, 50];
+            case 'tar'
+                cls = [ 1, 2, 3, 4, 5;...
+                    6, 7, 8, 9, 10; ...
+                    47, 48, 3, 49, 50;...
+                    35, 36, 8, 37, 38];
+        end
+    case 2
+        switch group
+            case 'den'
+                cls = [ 6, 7, 8, 9, 10; 1, 2, 3, 4, 5];
+            case 'con'
+                cls = [ 33, 34, 7, 35, 36; 45, 46, 2, 47, 48];
+            case 'tar'
+                cls = [ 1, 2, 3, 4, 5;...
+                    6, 7, 8, 9, 10;...
+                    45, 46, 2, 47, 48;...
+                    33, 34, 7, 35, 36];
+        end
+    case {3,4}
+        switch group
+            case 'den'
+                cls = [ 26, 27, 28, 29; 39, 38, 37, 36];
+            case 'con'
+                cls = [ 9, 10, 11, 12; 34, 33, 32, 31];
+            case 'tar'
+                cls = [ 39, 38, 37, 36;...
+                    26, 27, 28, 29;...
+                    34, 33, 32, 31;...
+                    9, 10, 11, 12 ];
+        end
+end
+end
+
+function [ Gabor_c, Gabor_s] =getFilters( mode, dataset)
+filter_cpd  = 3; % the images were band-passed at 3 cycles per degree
+fovs        = 12.5 * [1, 3/2, 1, 1]; % deg (the second data set had a larger field of view than the others)
+fov         = fovs(dataset);
+numpix      = 150;
+pixperdeg   = numpix / fov;
+ppc         = pixperdeg/filter_cpd; % pixels per cycle
+support     = 6; % cycles per filter
+o = linspace(0,pi, 9);
+thetavec = o(1:end-1);
+% Get filters
+switch  mode
+    case 'new'
+        [ Gabor_c, Gabor_s]=makeGaborFilter(ppc, thetavec, support);
+    case 'old'
+        [ Gabor_c, Gabor_s] = makeOldFilter(ppc, thetavec, support);
+end
 end
 
 function E = cal_oE( data, labelVec, mode, which_data)
